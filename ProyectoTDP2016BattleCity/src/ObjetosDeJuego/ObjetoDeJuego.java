6a2f9ed43367ed10c725638673ce1bc8b120e99e
leovolpe <leandrovolpe@hotmail.com>
2016-09-12 16:06:14 -0300

    creacion de la clase que mantiene la posicion de los objetos del juego y su jlabel

---
+++ ProyectoTDP2016BattleCity/src/ObjetosDeJuego/ObjetoDeJuego.java
@@ -0,0 +1,33 @@
+package ObjetosDeJuego;
+
+import javax.swing.JLabel;
+
+public class ObjetoDeJuego {
+	
+	private int x;
+	private int y;
+	private JLabel etiqueta;
+	
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public JLabel getEtiqueta() {
+		return etiqueta;
+	}
+	public void setEtiqueta(JLabel etiqueta) {
+		this.etiqueta = etiqueta;
+	}
+	
+	
+
+}
